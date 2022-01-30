80297ec85a43c55345632ccd09b5de4d5c18ab41
saikinkatenai95 <saikinkatenai95@outlook.jp>
2022-01-30 20:43:45 +0900

    controller 設定

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,42 @@
 class IdeasController < ApplicationController
+  before_action :set_category
+  before_action :set_category_idea, only: [:show, :update, :destroy]
+
+  def index
+    json_response(@category.ideas)
+  end
+
+  def show
+    json_response(@idea)
+  end
+
+  def create
+    @category.ideas.create!(idea_params)
+    json_response(@category, :created)
+  end
+
+  def update
+    @idea.update(idea_params)
+    head :no_content
+  end
+
+  def destroy
+    @idea.destroy
+    head :no_content
+  end
+
+  
+
+  private
+  def idea_params
+    params.permit(:body)
+  end
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def set_category_idea
+    @idea = @category.ideas.find_by!(id: params[:id]) if @category
+  end
 end
